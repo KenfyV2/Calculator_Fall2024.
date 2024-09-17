8e5ee51f1e2dcc95f4260c001953829a7e5e2b9a
Andrew <andrewlmw3@gmail.com>
2024-09-17 17:19:22 -0400

    Added Calculator Functions

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,22 @@
 ''' My Calculator Test'''
-from app.main import addition
+import pytest # type: ignore
+from app.main import addition, division, subtraction, multiplication
 
 def test_addition():
     '''Addition funtion'''
     assert addition(1,1)  == 2
+
+def test_subtraction():
+    '''Subtraction funtion'''
+    assert subtraction(1,1)  == 0
+
+def test_multiplication():
+    assert multiplication(2,2) == 4
+
+def test_division():
+    assert division(2,2) == 1
+
+def test_division_by_zero_exception():
+    '''Test division to see if i get the exception by zero'''
+    with pytest.raises(ZeroDivisionError):
+        division(10, 0)
